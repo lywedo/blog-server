839987775f8137bedc2c5ecfa0c8708e28e3fccc
Yu Lin <lywedo@gmail.com>
2020-12-22 10:06:59 +0800

    https?

--- src/main/java/com/dream/BlogApplication.java
+++ src/main/java/com/dream/BlogApplication.java
@@ -1,16 +1,31 @@
 package com.dream;
 
+import org.apache.catalina.connector.Connector;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @MapperScan("com.dream.mapper")
 @SpringBootApplication
 public class BlogApplication {
-
-    public static void main(String[] args) {
+//    @Bean
+//    public ServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+//        return tomcat;
+//    }
+//
+//    private Connector createStandardConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setPort(8080);
+//        return connector;
+//    }
+        public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
 
