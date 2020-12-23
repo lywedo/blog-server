9b9a009e9f627151887ef43ce81b268aae5e5c8a
Yu Lin <lywedo@gmail.com>
2020-12-23 19:32:22 +0800

    fix https probelm

--- src/main/java/com/dream/BlogApplication.java
+++ src/main/java/com/dream/BlogApplication.java
@@ -13,20 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @MapperScan("com.dream.mapper")
 @SpringBootApplication
 public class BlogApplication {
-//    @Bean
-//    public ServletWebServerFactory servletContainer() {
-//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-//        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-//        return tomcat;
-//    }
-//
-//    private Connector createStandardConnector() {
-//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//        connector.setPort(8080);
-//        return connector;
-//    }
         public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
-
 }
